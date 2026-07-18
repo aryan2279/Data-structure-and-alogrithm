7ef732f15d29c4a9839adaf2a37b10c439c2c4af
aryan2279 <yadavaryan9921@gmail.com>
2026-07-19 00:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (55.66%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findGCD(self, nums):
+        mi=min(nums)
+        ma=max(nums)
+        a=[]
+        for i in range(1,mi+1):
+            if mi%i==0 and ma%i==0:
+                a.append(i)
+        return max(a)
+
+
+        
